052992dc72e0fe9c8d925b23b16ea5bb9fb69036
Erfan Karimi <74706544+MR-MKZ@users.noreply.github.com>
2021-12-03 15:25:18 +0330

    Update InviteLogger.py

--- cogs/InviteLogger.py
+++ cogs/InviteLogger.py
@@ -68,10 +68,10 @@ class InviteLogger(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        inviter = await self.tracker.fetch_inviter(member)
+        
         channel = discord.utils.get(member.guild.text_channels, name="bot-commands")
         data = await self.client.invites.find_by_custom(
-            {"guild_id": member.guild.id, "inviter_id": inviter.id}
+            {"guild_id": member.guild.id}
         )
         try:
             data["count"] -= 1
